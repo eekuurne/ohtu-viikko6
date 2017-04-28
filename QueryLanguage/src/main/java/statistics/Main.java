cf129a219a25982509ccd22166dd2aa182e9e535
Eero Kuurne <eero.kuurne@helsinki.fi>
2017-04-28 22:36:08 +0300

    Exercise 4

--- QueryLanguage/src/main/java/statistics/Main.java
+++ QueryLanguage/src/main/java/statistics/Main.java
@@ -3,16 +3,28 @@ package statistics;
 import statistics.matcher.*;
 
 public class Main {
+
     public static void main(String[] args) {
-        Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
-          
-        Matcher m = new Not( new HasFewerThan(10, "goals"),
-                             new HasFewerThan(10, "assists"),
-                             new PlaysIn("PHI")
-        );
+        Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstatistics.herokuapp.com/players.txt"));
+
+        QueryBuilder query = new QueryBuilder();
+
+        Matcher m1 = query.playsIn("PHI")
+                .hasAtLeast(10, "goals")
+                .hasFewerThan(20, "assists").build();
+
+        query = new QueryBuilder();
         
+        Matcher m2 = query.playsIn("NYR")
+                     .hasAtLeast(10, "goals")
+                     .hasFewerThan(25, "assists").build();
+
+        query = new QueryBuilder();
+        
+        Matcher m = query.oneOf(m1, m2).build();
+
         for (Player player : stats.matches(m)) {
-            System.out.println( player );
+            System.out.println(player);
         }
     }
 }
